4b5387cb26f17b4bb3f199d499f7b18aef0406bc
alex <alexsandro.sousa12366@gmail.com>
2021-06-13 16:50:38 -0300

    add Header

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -21,16 +21,12 @@ export default {
       {
         slug: 'trending',
         title: 'Recomendados para você',
-        items: await basicFetch(
-          `/trending/all/week?language=pt-BR&api_key=${API_KEY}`,
-        ),
+        items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`),
       },
       {
         slug: 'toprated',
         title: 'Em Alta',
-        items: await basicFetch(
-          `/movie/top_rated?language=pt-BR&api_key=${API_KEY}`,
-        ),
+        items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`),
       },
       {
         slug: 'action',
@@ -68,5 +64,24 @@ export default {
         ),
       },
     ];
+  },
+  getMovieInfo: async (movieId, type) => {
+    let info = {};
+
+    if (movieId) {
+      switch (type) {
+        case 'movie':
+          info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+          break;
+        case 'tv':
+          info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+          break;
+        default:
+          info = null;
+          break;
+      }
+    }
+
+    return info;
   },
 };
